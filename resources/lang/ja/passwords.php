a01c97e41454b51492ec27e2fb610cddc32e9c71
kiwi-26 <winter-wind.sh@hotmail.co.jp>
2020-06-18 02:09:26 +0900

    日本語対応

---
+++ resources/lang/ja/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'パスワードがリセットされました。',
+    'sent' => 'パスワードリセット用のメールをメールアドレス宛てに送信しました。',
+    'throttled' => 'しばらく経ってから再度お試しください。',
+    'token' => 'このパスワードリセットトークンは使用できません。',
+    'user' => "このメールアドレスのユーザーが見つかりませんでした。",
+
+];
