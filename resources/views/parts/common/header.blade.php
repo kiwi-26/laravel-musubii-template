2b04bc1adf24df80c4db09c8e39cada09b1e9643
kiwi-26 <winter-wind.sh@hotmail.co.jp>
2020-06-17 20:27:00 +0900

    auth ヘッダーを追加、デザイン調整

--- resources/views/parts/common/header.blade.php
+++ resources/views/parts/common/header.blade.php
@@ -1,2 +1,15 @@
 <header class="inner">
+@if (Route::has('login'))
+    <div class="box is-flex is-space-left-sm is-right is-margin-vertical-md">
+        @auth
+        <a class="text is-link is-primary" href="{{ url('/home') }}">Home</a>
+        @else
+        <a class="text is-link is-primary" href="{{ route('login') }}">Login</a>
+
+        @if (Route::has('register'))
+        <a class="text is-link is-primary" href="{{ route('register') }}">Register</a>
+        @endif
+        @endauth
+    </div>
+@endif
 </header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,15 +14,15 @@ Laravel
         <h1 class="text is-xxl is-center">Laravel</h1>
     </div>
     <div class="column is-12">
-        <div class="grid is-gap is-center">
-            <span class="column"><a class="text is-link" href="https://laravel.com/docs">Docs</a></span>
-            <span class="column"><a class="text is-link" href="https://laracasts.com">Laracasts</a></span>
-            <span class="column"><a class="text is-link" href="https://laravel-news.com">News</a></span>
-            <span class="column"><a class="text is-link" href="https://blog.laravel.com">Blog</a></span>
-            <span class="column"><a class="text is-link" href="https://nova.laravel.com">Nova</a></span>
-            <span class="column"><a class="text is-link" href="https://forge.laravel.com">Forge</a></span>
-            <span class="column"><a class="text is-link" href="https://vapor.laravel.com">Vapor</a></span>
-            <span class="column"><a class="text is-link" href="https://github.com/laravel/laravel">GitHub</a></span>
+        <div class="box is-flex is-space-right-sm is-center is-margin-right-minus-sm">
+            <a class="text is-link" href="https://laravel.com/docs">Docs</a>
+            <a class="text is-link" href="https://laracasts.com">Laracasts</a>
+            <a class="text is-link" href="https://laravel-news.com">News</a>
+            <a class="text is-link" href="https://blog.laravel.com">Blog</a>
+            <a class="text is-link" href="https://nova.laravel.com">Nova</a>
+            <a class="text is-link" href="https://forge.laravel.com">Forge</a>
+            <a class="text is-link" href="https://vapor.laravel.com">Vapor</a>
+            <a class="text is-link" href="https://github.com/laravel/laravel">GitHub</a>
         </div>
     </div>
 </div>
